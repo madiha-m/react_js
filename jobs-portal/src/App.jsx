783e069183aa4e726c7ec6814acbb273b6a0d87c
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-03 23:48:30 +0500

    ternary operator ? replace by && needs to check only true case, and else part has empty string

--- jobs-portal/src/App.jsx
+++ jobs-portal/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const App = () => {
+  const x = 20;
+  const y = 30;
+  const names = ['Brad', 'Mary', 'Sara'];
+  const loggedIn = true;
+
   return (
-    <div className='text-2xl'>App</div>
+    <>
+      <div className='text-2xl'>App</div>
+      <p>the sum of {x} and {y} is = {x + y}.</p>
+
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+
+      {loggedIn ? <h1>hi this is true.</h1> : ""}
+      <p>Ternary operator can replace by && if else condition has noting</p>
+      {loggedIn && <h1>the other part is false, its true part. </h1>}
+      
+    </>
   )
 }
 
