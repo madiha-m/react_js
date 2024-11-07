40db66850b1d2115443f4a4dc86fcc4d3ad04799
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-07 21:22:32 +0500

    remove extra comments from App.jsx file to make it more clean

--- jobs-portal/src/App.jsx
+++ jobs-portal/src/App.jsx
@@ -8,22 +8,11 @@ import ViewAllJobs from './components/ViewAllJobs'
 const App = () => {
   return (
     <>
-      {/* ---Navbar--- */}
-      {/* import Navbar.jsx component, and paste Nav code in that file */}
+
       <Navbar />
-
-      {/* ---Hero--- */}
-      {/* set Hero component, then pass props to the child>Hero.jsx from parent>App.jsx */}
       <Hero title='Become a React Dev' subtitle='Find the React job that fits your skills and needs' />
-
-      {/* ---Developers and Employers--- */}
-      {/* Creating HomeCards as like above components */}
       <HomeCards />
-
-      {/* ---Browse Jobs--- */}
       <JobListings />
-
-      {/* ---View All Jobs--- */}
       <ViewAllJobs />
     </>
   )
