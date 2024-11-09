b992deb7d2b117032f6e62c3a7aab9ff95fada28
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-10 00:57:20 +0500

    use of general componenet like navbar, shoudl use above Outlet if above

--- jobs-portal/src/App.jsx
+++ jobs-portal/src/App.jsx
@@ -1,11 +1,14 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import React from 'react'
 import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route index element={<HomePage />} />
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
     )
   )
 
