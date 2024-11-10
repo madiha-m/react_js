f0df0dc7eda79b9ad8da13c49c4401f96d6e7310
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-10 23:42:32 +0500

    Creet custom 404 page, add its router, give * as path to its router

--- jobs-portal/src/App.jsx
+++ jobs-portal/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
   const router = createBrowserRouter(
@@ -10,6 +11,7 @@ const App = () => {
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
   )
