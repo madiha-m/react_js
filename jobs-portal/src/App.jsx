1c479aaaa923440054b995456643fd4741aca611
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-10 23:22:26 +0500

    create other pages in pages dir, add the page route in App.jsx, us of <Link> by replacing <a> including href with to in NavBar and other componenets. Link not refresh teh page on every click as linke anchor tag

--- jobs-portal/src/App.jsx
+++ jobs-portal/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import React from 'react'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
       </Route>
     )
   )
