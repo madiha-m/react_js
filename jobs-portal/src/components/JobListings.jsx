429c72a34b6f9a6e5bcae5e1973603a137d4d2b3
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-13 00:56:13 +0500

    useEffect() hook & Data Fetching from JSON server, use of pre-loader

--- jobs-portal/src/components/JobListings.jsx
+++ jobs-portal/src/components/JobListings.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import jobs from '../jobs.json'
+import React, { useEffect, useState } from 'react'
 import JobListing from './JobListing'
 
 const JobListings = ({ isHome = false }) => {
-    // console.log(jobs); 
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    // limit number of jobs showing on the home page
-    const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const res = await fetch('http://localhost:8000/jobs');
+                const data = await res.json();
+                setJobs(data);
+            }
+            catch (error) {
+
+                console.log('Error fetching data', error);
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        fetchJobs();
+    }, []);
 
     return (
         <section className="bg-blue-50 px-4 py-10">
@@ -16,8 +31,7 @@ const JobListings = ({ isHome = false }) => {
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-                    {/* replacing Jobs array via map */}
-                    {jobListings.map((job) => (
+                    {jobs.map((job) => (
                         <JobListing key={job.id} job={job} />
                     ))}
                 </div>
