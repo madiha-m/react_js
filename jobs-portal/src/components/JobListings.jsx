2754b3a6a00d6f91e6e0d54a77bd21383fea5334
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-13 22:59:55 +0500

    Issue: Spinner not in the middle of page. the map() with spinner  enclosed in its div, apply div only on the map.

--- jobs-portal/src/components/JobListings.jsx
+++ jobs-portal/src/components/JobListings.jsx
@@ -30,17 +30,15 @@ const JobListings = ({ isHome = false }) => {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? "Recent Jobs" : "Browse Jobs"}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {loading ? (
-                        <Spinner Loading={loading} />
-                    ) : (
-                        <>
-                            {jobs.map((job) => (
-                                <JobListing key={job.id} job={job} />
-                            ))}
-                        </>
-                    )}
-                </div>
+                {loading ? (
+                    <Spinner Loading={loading} />
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {jobs.map((job) => (
+                            <JobListing key={job.id} job={job} />
+                        ))}
+                    </div>
+                )}
             </div>
         </section>
     )
