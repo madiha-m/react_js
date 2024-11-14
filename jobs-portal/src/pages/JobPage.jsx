1570b7dd101b45cd971c030e77eba694b4e5c0c1
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-15 01:41:33 +0500

    fetch data using data loader, and make it global state

--- jobs-portal/src/pages/JobPage.jsx
+++ jobs-portal/src/pages/JobPage.jsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 
 const JobPage = () => {
     const { id } = useParams();
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(`/api/jobs/${id}`);
-                const data = await res.json();
-                setJob(data);
-            }
-            catch (error) {
-                console.log('Error fetching data', error);
-            } finally {
-                setLoading(false);
-            }
-        }
-        fetchJob()
-    }, [])
+    // const [loading, setLoading] = useState(true);
+
 
     return (
         loading ? <Spinner /> :
@@ -30,4 +15,10 @@ const JobPage = () => {
     )
 }
 
-export default JobPage
+const jobLoader = async ({ params }) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
+}
+
+export { JobPage as default, jobLoader };
