a25f6fb09b6fe71e042492e3eb15d96397663085
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-19 23:37:52 +0500

    addJobSubmit fun should pass as param for the AddJobPage in element, outside giving error 'AddJobPage.jsx:34 Uncaught TypeError: addJobSubmit is not a function' in console

--- jobs-portal/src/App.jsx
+++ jobs-portal/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/add-job' element={<AddJobPage />} addJobSubmit={addJob} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}  />
         <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
