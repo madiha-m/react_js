2e7eb4f5c4d6353bba658ef522550c97332108dd
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-25 21:37:50 +0500

    apply useTheme() hook from context lib, apply condition with theme var of the hook to sgow logo based on light/dark mode.

--- Climate/src/components/header.tsx
+++ Climate/src/components/header.tsx
@@ -1,12 +1,15 @@
+import { useTheme } from '@/context/theme-provider'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const { theme } = useTheme();
+
     return (
         <header className='sticky top-0 z-50 border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60'>
             <div className='container mx-auto flex h-16 items-center justify-between px-4'>
                 <Link to={'/'}>
-                    <img src='/logo.png'
+                    <img src={theme === "dark" ? '/logo.png' : "/logo2.png"}
                         alt='Climate Logo'
                         className='h-12'
                     />
