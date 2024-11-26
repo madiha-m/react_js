c04bb714433f3e9cc67baf3ee34a9b2214847e2f
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-27 00:36:31 +0500

    craeteUrl() having endpoints and params as params. Convert params into SearchParams. return endpoint with searchparam

--- Climate/src/api/weather.ts
+++ Climate/src/api/weather.ts
@@ -1,5 +1,17 @@
+import { API_CONFIG } from "./config"
+
 class WeatherAPI {
-    private createApi() { }
+    private createUrl(
+        endpoint: string,
+        params: Record<string, string | number>
+    ) {
+        const searchParams = new URLSearchParams({
+            appid: API_CONFIG.API_KEY,
+            ...params,
+
+        })
+        return `${endpoint}?${searchParams.toString()}`
+    }
     private fetchData() { }
     async getCurrentWeather() { }
     async getForecast() { }
