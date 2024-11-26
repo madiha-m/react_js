8f9610ce981d36384291d155e251dd162148350e
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-26 12:17:48 +0500

    setup react query

--- Climate/src/App.tsx
+++ Climate/src/App.tsx
@@ -4,21 +4,27 @@ import Layout from './components/layout'
 import { ThemeProvider } from './context/theme-provider'
 import Dashboard from './pages/dashboard'
 import CityPage from './pages/city-page'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
     <>
-      <BrowserRouter>
-        <ThemeProvider defaultTheme='dark'>
-          <Layout>
-            <Routes>
-              <Route path='/' element={<Dashboard />} />
-              <Route path='/city/:cityName' element={<CityPage />} />
-            </Routes>
-          </Layout>
-        </ThemeProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider defaultTheme='dark'>
+            <Layout>
+              <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/city/:cityName' element={<CityPage />} />
+              </Routes>
+            </Layout>
+          </ThemeProvider>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false}/>
+      </QueryClientProvider>
     </>
   )
 }
