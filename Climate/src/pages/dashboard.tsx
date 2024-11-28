3a13f68eacfb3f8cf24319f0ebf7438f9c6bf4ed
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-28 21:14:14 +0500

    add shadcn/ui Button add resresh-icon from lucid-re4act lib, apply styling on Button and give h+w to the icon

--- Climate/src/pages/dashboard.tsx
+++ Climate/src/pages/dashboard.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+import { RefreshCw } from 'lucide-react'
 import React from 'react'
 
 const Dashboard = () => {
@@ -6,7 +8,13 @@ const Dashboard = () => {
             {/* Favorite Cities */}
             <div>
                 <h1>My Location</h1>
-                {/* Refresh Icon */}
+                <Button variant={'outline'}
+                    size={"icon"}
+                // onClick={handleRefresh}   // fun not exist right now
+                // disabled={ }              // will be disabled while fetching current/forecast data
+                >
+                    <RefreshCw className='h-4 w-4' />
+                </Button>
             </div>
             {/* Current and Hourly weather */}
         </div>
