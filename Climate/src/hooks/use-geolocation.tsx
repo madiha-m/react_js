036877dff2d37215217204128fb8286bb6a39308
madiha-m <madiha.muhammad.dev@gmail.com>
2024-11-30 21:37:38 +0500

    use default JS API Navigatitor, to get data. Apply condition in case if not getting data and setLocatioonData if get the data

--- Climate/src/hooks/use-geolocation.tsx
+++ Climate/src/hooks/use-geolocation.tsx
@@ -11,6 +11,25 @@ export function useGeolocation() {
 
     const getLocation = () => {
         setLocationData((prev) => ({ ...prev, isLoading: true, error: null }));
+
+        if (!navigator.geolocation) {
+            setLocationData({
+                coordinates: null,
+                error: "GeoLocation is not supported by the browser.",
+                isLoading: false,
+            })
+            return;
+        }
+        navigator.geolocation.getCurrentPosition((position) => {
+            setLocationData({
+                coordinates: {
+                    lon: position.coords.longitude,
+                    lat: position.coords.latitude,
+                },
+                error: null,
+                isLoading: false,
+            });
+        });
     };
 
     useEffect(() => {
