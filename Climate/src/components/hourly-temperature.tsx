77046265ee0d85f3e2188bbb08c0aaecc8a9ce9c
madiha-m <madiha.muhammad.dev@gmail.com>
2024-12-30 03:44:08 +0500

    installed fadte-fns lib. created fun for filling hourly temp. data.

--- Climate/src/components/hourly-temperature.tsx
+++ Climate/src/components/hourly-temperature.tsx
@@ -1,17 +1,36 @@
 import { HourlyTemperatureProps } from '@/api/types'
 import React from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
+import { Line, LineChart, ResponsiveContainer } from 'recharts'
+import { format } from 'date-fns'
 
 const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
+  const chartData = data.list.slice(0, 8).map((item) => ({
+    time: format(new Date(item.dt * 1000), "ha"),
+    temp: Math.round(item.main.temp),
+    feels_like: Math.round(item.main.feels_like)
+  }))
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>
-          {/* title as Today's Temperature */}
+          Today's Temperature
         </CardTitle>
       </CardHeader>
       <CardContent>
-        {/* holding card content */}
+        <div className="h-[200px] w-full">
+          <ResponsiveContainer
+            width={'100%'}
+            height={'100%'}
+          >
+            <LineChart
+              data={chartData}
+            >
+              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
       </CardContent>
     </Card>
   )
