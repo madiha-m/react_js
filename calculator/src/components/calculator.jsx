51af44e455b636f7f3e0116b514a315a2f55924b
madiha-m <madiha.muhammad.dev@gmail.com>
2025-02-17 11:10:14 +0500

    Styled result box

--- calculator/src/components/calculator.jsx
+++ calculator/src/components/calculator.jsx
@@ -25,7 +25,10 @@ const calculator = () => {
   ];
 
   return (
-    <div>
+    <div className="min-w-[320px] bg-black flex flex-col gap-4 p-4 rounded-2xl">
+      <div className="overflow-x-auto bg-[#141414] min-h-[100px] flex items-end justify-end flex-col p-4 rounded-[10px]">
+        <div>RESULT</div>
+      </div>
       <div>
         {keys.map((item, index) => (
           <Keys
