806b42610bc074094f8d2f7774cd7029ae925f2d
madiha-m <madiha.muhammad.dev@gmail.com>
2025-02-17 11:01:04 +0500

    styled the calculator

---
+++ calculator/src/components/calculator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const calculator = () => {
+  return <div>calculator</div>;
+};
+
+export default calculator;
---
+++ calculator/src/components/keys.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Keys = ({ label }) => {
+  return <div>keys</div>;
+};
+
+export default Keys;
