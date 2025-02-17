a6241e54b5070073ad73ad852dd1cdefff495aa6
madiha-m <madiha.muhammad.dev@gmail.com>
2025-02-17 11:03:34 +0500

    add keys

--- calculator/src/components/calculator.jsx
+++ calculator/src/components/calculator.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Keys from "./keys";
 
 const calculator = () => {
-  return <div>calculator</div>;
+  const keys = [
+    "AC",
+    "C",
+    "%",
+    "/",
+    "*",
+    "-",
+    "+",
+    "0",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
+    ".",
+    "EQUALS",
+  ];
+
+  return (
+    <div>
+      <div>
+        {keys.map((item, index) => (
+          <Keys
+            label={item}
+            key={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default calculator;
