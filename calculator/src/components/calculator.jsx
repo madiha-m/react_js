a8558c3781ef874476942239063fd630b331fec4
madiha-m <madiha.muhammad.dev@gmail.com>
2025-02-21 03:11:48 +0500

    The result/display sectionfunctionality applied

--- calculator/src/components/calculator.jsx
+++ calculator/src/components/calculator.jsx
@@ -8,7 +8,7 @@ const Calculator = () => {
     "(",
     ")",
     "%",
-    "CE",
+    "AC",
     "Inv",
     "sin",
     "ln",
@@ -41,6 +41,7 @@ const Calculator = () => {
 
   const [showResult, setShowResult] = useState(false);
   const [display, setDisplay] = useState("");
+  const [result, setResult] = useState("");
   const [isRad, setIsRad] = useState(false);
 
   const maxLimit = 15;
@@ -60,33 +61,42 @@ const Calculator = () => {
 
   function handleButton(value) {
     setShowResult(false);
-    if (value === "AC") setDisplay("");
-    else if (value === "C") setDisplay(display.slice(0, -1));
+    if (value === "AC") setDisplay(display.slice(0, -1));
     else if (isOperator(value)) {
       if (display == "" || isOperator(display[display.length - 1])) return;
       setDisplay(display + value);
     } else if (value === "=") calculateResult();
     else if (value === "Rad | Deg") {
       setIsRad(!isRad);
-    } else if (display.length >= maxLimit)
+    } else if (display.length >= maxLimit) {
       alert(`maximum characters allowed : ${maxLimit}`);
-    else setDisplay(display + value);
+    } else {
+      if (display === "0" || display === "") {
+        setDisplay(value);
+      } else if (display === "Error") {
+        setDisplay(value);
+      } else setDisplay(display + value);
+    }
   }
 
   function isOperator(char) {
     return ["*", "/", "%"].includes(char);
   }
 
-  const operationClass =
+  // const operationClass =
+  //   "text-[1.2rem] tracking-[2px] flex gap-[5px] items-center text-[rgba(255,255,255,0.5)] justify-end";
+  // const resultClass = "text-[1.7rem] text-white";
+
+  const resultClass =
     "text-[1.2rem] tracking-[2px] flex gap-[5px] items-center text-[rgba(255,255,255,0.5)] justify-end";
-  const resultClass = "text-[1.7rem]";
+  const operationClass = "text-[1.7rem] text-white";
 
   return (
     <div className="min-w-[320px] bg-black flex flex-col gap-4 p-4 rounded-2xl">
       <div className="overflow-x-auto bg-[#141414] min-h-[100px] flex items-end justify-end flex-col p-4 rounded-[10px]">
-        <div className={`${showResult ? resultClass : operationClass}`}>
-          {display}
-        </div>
+        <div className={resultClass}>vvv</div>
+        {/* <div className={`${showResult ? operationClass : operationClass}`}> */}
+        <div className={operationClass}>{display || "0"}</div>
       </div>
       <div className="grid grid-cols-[repeat(7,1fr)] gap-[0.3rem]">
         {keys.map((item, index) => (
